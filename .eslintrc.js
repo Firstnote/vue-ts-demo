f3f06903855240acac6893464c3b1dd4d2a98c49
Firstnote <965762739@qq.com>
2020-03-20 14:05:46 +0800

    eslint-test

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,17 @@ module.exports = {
 	},
 	parserOptions: {
     sourceType: "module",
-		ecmaVersion: 2015
-		// parser:"@typescript-eslint/parser"
+		ecmaVersion: 2015,
+		parser:"@typescript-eslint/parser"
 	},
-	extends: "eslint:recommended",
+	plugins:["fnlint"],
+	extends: ["plugin:vue/essential"],
 	rules: {
 		// "no-hello-in-identifier": 2,
-		// "no-console": 2,
+		"no-console": 2,
 		"no-undef": "off",
-		"no-$ajax-inuse": 1,
-		"no-reqlocal":1
+		"fnlint/no-xmlrequest":"error"
+		// "no-$ajax-inuse": 1,
+		// "no-reqlocal":1
 	}
 };
